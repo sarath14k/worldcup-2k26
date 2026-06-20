d0c7f9188bfabef227bb79996d3ba34fd133a391
sarath14k <sarathudaya71@gmail.com>
2026-06-21 01:25:39 +0530

    fix: unregister service worker and self-destruct to clear stale browser cache

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,19 @@
-const CACHE_NAME = 'worldcup-predictor-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon.svg'
-];
-
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => caches.delete(key))
+      );
+    }).then(() => {
+      return self.clients.claim();
     })
   );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
