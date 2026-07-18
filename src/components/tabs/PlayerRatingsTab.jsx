feb419a1039480de7ee7c6aed3531ce05d2aeee5
sarath14k <sarathudaya71@gmail.com>
2026-07-18 22:40:25 +0530

    fix: add missing memo import in PlayerRatingsTab

--- src/components/tabs/PlayerRatingsTab.jsx
+++ src/components/tabs/PlayerRatingsTab.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, memo } from 'react';
 import { Search, X, Star, Filter } from 'lucide-react';
 import { TEAMS } from '../../data/worldcupData';
 import { ScrollingText } from '../ScrollingText';
